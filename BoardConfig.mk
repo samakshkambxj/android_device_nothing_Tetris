7c975937e0d340b4f034ae3e43db0b4f985067b2
J. Avila <elavila@google.com>
2021-04-12 18:22:36 +0000

    Tetris: Explicitly set log_buf_len
    By setting the log_buf_len via cmdline, devices using this cmdline will
    be able to guarantee their preferred buffer size even if the underlying
    CONFIG_LOG_BUF_SHIFT changes.
    
    Bug: 185141673
    Change-Id: Iafe7561dceef65fc47cb0bb54a8f84a5e10c51a9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 
 BOARD_KERNEL_CMDLINE += bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += cgroup_disable=memory
+BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 BOARD_KERNEL_CMDLINE += rcu_nocbs=all rcutree.enable_rcu_lazy=1
 
 BOARD_INIT_BOOT_HEADER_VERSION := 4
