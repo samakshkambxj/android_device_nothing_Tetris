8c3375ffd3bc14dbb89b6481a69fd64e42fa8f21
bengris32 <bengris32@protonmail.ch>
2023-12-15 21:07:23 +0000

    Tetris: Add shim for MediaTek AudioTrack constructor into `libsink-mtk`
    Change-Id: Icc7a1f9c748578f5b58499e7a76213673c4bcf54
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -34,6 +34,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'system_ext/lib64/libsink-mtk.so': blob_fixup()
+        .add_needed('libaudioclient_shim.so'),
     'vendor/bin/hw/vendor.noth.hardware.charge-service': blob_fixup()
         .add_needed('libbase_shim.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
