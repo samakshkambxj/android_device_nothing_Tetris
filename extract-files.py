21621d45bc31345e5e73f809878d9fd7f5d2d8d2
Giovanni Ricca <giovanniricca@proton.me>
2024-06-16 16:36:20 +0200

    Tetris: Patch libsource to load the old graphicsbuffer symbol
    Change-Id: Ic8f1880379bf7adf07193842a710a104e434d470
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -36,6 +36,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libsink.so', 'libsink-mtk.so'),
     'system_ext/lib64/libsink-mtk.so': blob_fixup()
         .add_needed('libaudioclient_shim.so'),
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
     'vendor/bin/hw/vendor.noth.hardware.charge-service': blob_fixup()
         .add_needed('libbase_shim.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
