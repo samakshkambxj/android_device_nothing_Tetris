52a64103032eb203fa1af9e4a99142f25765d48c
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-28 03:11:52 +0200

    Tetris: Implement richtap vibrator service
    Change-Id: Ieab0cfca22c0fd8a63c33ce2a6a21e982e57aee8
    
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- device.mk
+++ device.mk
@@ -271,6 +271,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator.service.Tetris-richtap
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndservicemanager
