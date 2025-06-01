53d7dbaaa9d1f19d6f03526b290e0e0d806cfad7
Alexander Koskovich <akoskovich@pm.me>
2025-06-01 07:44:16 -0400

    Tetris: Add `libnativewindow` to `libntcamskia`
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -96,6 +96,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_unlock'),
     'vendor/lib64/mt6878/libneuron_adapter_mc.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_describe'),
+    'vendor/lib64/libntcamskia.so': blob_fixup()
+        .add_needed('libnativewindow.so'),
     'vendor/lib64/libnvram.so': blob_fixup()
         .add_needed('libbase_shim.so'),
 }  # fmt: skip
