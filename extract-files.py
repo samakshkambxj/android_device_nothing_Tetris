02edc6012029067be7bba442a69e66117d96eae1
Mashopy <eliasgheeraert@gmail.com>
2025-06-17 21:22:35 +0200

    Tetris: Import LineageOS extract utils
    Change-Id: I4dc096dc3c935c8a95c4119690ebbf468eddd613
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- extract-files.py
+++ extract-files.py
@@ -4,18 +4,40 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
 namespace_imports = [
-    'vendor/nothing/Tetris',
+    'device/nothing/Tetris',
+    'hardware/mediatek',
 ]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+}  # fmt: skip
 
 module = ExtractUtilsModule(
     'Tetris',
     'nothing',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
 
