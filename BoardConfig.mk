39b31995c9bfb41909bbf79eb529e02e8eba6233
Mashopy <eliasgheeraert@gmail.com>
2025-06-18 15:38:49 +0200

    Tetris: Import vendor vintf manifest and compatiblity matrix
    Co-authored-by: sreelekshman <sreelekshmanchunakara@gmail.com>
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,11 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
+
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x3fff8000
