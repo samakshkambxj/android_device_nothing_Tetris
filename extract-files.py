8069386cc97d0889e53f6e5508992774edcf3803
Mashopy <eliasgheeraert@gmail.com>
2025-06-18 14:30:49 +0200

    Tetris: Clear a LIBNATIVEWINDOW symbol for `libneuron_adapter_mc`
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -94,6 +94,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lockPlanes')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/mt6878/libneuron_adapter_mc.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_describe'),
     'vendor/lib64/libnvram.so': blob_fixup()
         .add_needed('libbase_shim.so'),
 }  # fmt: skip
