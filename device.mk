b1673636073517884dd785b4c602ae58fce1873b
Mashopy <eliasgheeraert@gmail.com>
2025-06-18 22:55:38 +0200

    Tetris: overlay: Initial device specific RRO overlays
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- device.mk
+++ device.mk
@@ -165,6 +165,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Overlays
+PRODUCT_PACKAGES += \
+    NothingFrameworksResTarget \
+    NothingSystemUIResTarget \
+    NothingTelephonyResTarget \
+    NothingWifiResTarget
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
