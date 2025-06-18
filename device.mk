6e347c5525e265655261433e021d30639912504b
Mashopy <eliasgheeraert@gmail.com>
2025-06-18 20:20:34 +0200

    Tetris: init: Move product SKU prop to early-init
    We want it to be set as soon as possible

--- device.mk
+++ device.mk
@@ -142,6 +142,7 @@ PRODUCT_PACKAGES += \
     init.mt6878.rc \
     init.mt6878.usb.rc \
     init.mtkgki.rc \
+    init.nothing.hw.rc \
     init.project.rc \
     init.sensor_2_0.rc \
     ueventd.mt6878.rc
