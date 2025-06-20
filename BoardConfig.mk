bd834cd65764217592b7b918fa5028c32b34458a
Mashopy <eliasgheeraert@gmail.com>
2025-06-20 12:36:25 +0200

    Tetris: Include MediaTek sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,6 +123,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# SEPolicy
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
