112779d81daeae6629beb47ab8a2d95ed4c99b45
Mashopy <eliasgheeraert@gmail.com>
2025-06-30 04:30:23 +0200

    Tetris: Initial AIDL fingerprint service
    Based over [1] and [2]
    
    [1]: https://github.com/Pong-Development/device_nothing_Pong/tree/5d68087a084ba3ea183e3d0db10c8459c764ab98/fingerprint
    [2]: https://github.com/SuperAviation001/android_device_nothing_tetris/tree/5bfec69fe167495f4fc958ac7c3a42ef10ec0457/fingerprint
    
    Notes:
    
    Goodix extCmd function has been entirely dropped since Pacman fingerprint HAL
    does not make use of it and we write to both the HBM node on mtk_drm driver and
    the HBM mode node on the drm panel driver intentionally since the fingerprint HAL
    expect this behavior (as seen on stock decompiled frameworks and surfaceflinger).
    
    The sensor props directly comes from Nothing fingerprint service aproximated to AOSP
    expectations as per logcat.
    
    This also allows us to drop entirely framework hack for writing to these nodes
    and to get a more reliable fingerprint sensor.
    
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- device.mk
+++ device.mk
@@ -126,6 +126,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.nothing
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
