aa8ec9546e4e01728452d43b40b184702209a4ce
Mashopy <eliasgheeraert@gmail.com>
2025-08-18 05:59:08 +0200

    Tetris: Remove checkpoint_gc
    Google stopped using it since gs101, the exact reason why is unknown since it was done during early development.

--- device.mk
+++ device.mk
@@ -16,12 +16,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=ext4 \
-    POSTINSTALL_OPTIONAL_vendor=true
-
 PRODUCT_PACKAGES += \
     com.android.hardware.boot \
     android.hardware.boot-service.default_recovery
@@ -31,7 +25,6 @@ PRODUCT_PACKAGES += \
     create_pl_dev.recovery
 
 PRODUCT_PACKAGES += \
-    checkpoint_gc \
     otapreopt_script \
     update_engine \
     update_engine_sideload \
