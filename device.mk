083c006b0dc7eac288f90c82287cabab314c3b60
Mashopy <eliasgheeraert@gmail.com>
2025-09-22 10:47:06 +0200

    Tetris: Enable `PRODUCT_NO_BIONIC_PAGE_SIZE_MACRO`
    This property is set to false by default on VSR 34 targets however, Nothing set that flag to true for both Pacman and PacmanPro.

--- device.mk
+++ device.mk
@@ -205,6 +205,9 @@ PRODUCT_PACKAGES += \
     LineageSettingsResTarget \
     LineageSystemUIResTarget
 
+# Page size
+PRODUCT_NO_BIONIC_PAGE_SIZE_MACRO := true
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
