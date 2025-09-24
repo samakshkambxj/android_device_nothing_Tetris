9f7835e35746ca757e732add75dc1c66bb6ea473
Mashopy <eliasgheeraert@gmail.com>
2025-09-24 13:08:57 +0200

    Tetris: init: Drop `init.modem.rc`
    RPS has been disabled entirely on the kernel, that vold property is for FDE and the rest isn't important.

--- device.mk
+++ device.mk
@@ -154,7 +154,6 @@ PRODUCT_PACKAGES += \
     init_conninfra.rc \
     init.connectivity.common.rc \
     init.connectivity.rc \
-    init.modem.rc \
     init.mt6878.rc \
     init.mt6878.power.rc \
     init.mt6878.usb.rc \
