09819cf7656d39eebf26b96895cd7fffc0ec9371
Mashopy <eliasgheeraert@gmail.com>
2025-12-08 10:06:58 +0100

    Tetris: Import merged CarrierConfig overlay from NothingOS 4.0

--- device.mk
+++ device.mk
@@ -196,6 +196,7 @@ PRODUCT_BOOT_JARS += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+    NothingCarrierConfigResTarget \
     NothingFrameworksResTarget \
     NothingNetworkStackResTarget \
     NothingSettingsResTarget \
