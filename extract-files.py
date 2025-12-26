83c58ced14371681043532de74ea8d1c4ad05413
Giovanni Ricca <giovanniricca@proton.me>
2025-12-26 15:03:42 +0100

    Tetris: Import system NPU libs
    Change-Id: Ic45ce534700410fdb462dd3315b0defdaf74bbd3
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -29,7 +29,12 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    'vendor.mediatek.hardware.videotelephony@1.0': lib_fixup_vendor_suffix,
+    (
+        'libneuron_graph_delegate.mtk',
+        'libtflite_mtk',
+        'vendor.mediatek.hardware.apuware.utils@2.0',
+        'vendor.mediatek.hardware.videotelephony@1.0'
+    ): lib_fixup_vendor_suffix,
 }
 
 blob_fixups: blob_fixups_user_type = {
