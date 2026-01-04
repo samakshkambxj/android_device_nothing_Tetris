7028dcffa623b458feb91d955d0d8b9cc41be108
Mashopy <eliasgheeraert@gmail.com>
2026-01-04 22:58:00 +0100

    Tetris: overlay: Enable MTK GHBM udfps impl and define the DimLayer name
    Required with latest changes done on frameworks/base

--- device.mk
+++ device.mk
@@ -127,6 +127,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Fingerprint
 $(call soong_config_set_bool,surfaceflinger,has_mtk_udfps,true)
+$(call soong_config_set,surfaceflinger,mtk_dim_layer,NTFingerprintDimLayer)
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.nothing
