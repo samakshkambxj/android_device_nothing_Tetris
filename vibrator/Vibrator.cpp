d812ef7f57b96ca544cd33e679db7c25bc45dd7e
hiroshi <hiroshi@tanuki.page>
2026-01-08 08:50:05 -0500

    Tetris: vibrator: Properly stop haptic effects
    Always call aac_vibra_looper_stopPerformHe() before playing a new effect, or else the vibrator will continue adding
    to the looper, causing effects to stack or play long after they should be.
    
    Also call it in Vibrator:off

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -51,7 +51,12 @@ ndk::ScopedAStatus Vibrator::getCapabilities(int32_t* _aidl_return) {
 }
 
 ndk::ScopedAStatus Vibrator::off() {
-    int32_t ret = aac_vibra_off();
+    bool ret = aac_vibra_looper_stopPerformHe();
+
+    if (ret)
+        ALOGW("No HE effects to stop!");
+
+    ret = aac_vibra_off();
     if (ret) {
         ALOGE("AAC off failed: %d\n", ret);
         return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_SERVICE_SPECIFIC));
@@ -113,6 +118,8 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength es,
 
     if (sLastMode == MODE_STREAM)
         aac_vibra_setAmplitude(0xFF);
+
+    aac_vibra_looper_stopPerformHe();
 
     int32_t ret = aac_vibra_looper_prebaked_effect(effectId, strength);
     if (ret < 0) {
