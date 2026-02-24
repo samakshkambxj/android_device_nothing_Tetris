f90c26f16f13b2ca2e1ae750608935374e3ff967
Mashopy <eliasgheeraert@gmail.com>
2026-02-24 01:42:54 +0100

    Tetris: Add LiveDisplay Sunlight Enhancement support
    Some users requested the possibility to toggle HBM manually.

--- device.mk
+++ device.mk
@@ -183,6 +183,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay-service.sysfs
+
+$(call soong_config_set_bool,livedisplay_sysfs,enable_se,true)
+$(call soong_config_set,livedisplay_sysfs,se_path,/sys/panel_feature/hbm_mode)
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.bufferpool2-V2-ndk.vendor \
