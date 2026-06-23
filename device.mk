b177d983504c5e37352fbe23f44f9b6760f91918
Michael Bestas <mkbestas@lineageos.org>
2026-06-24 01:57:14 +0300

    Tetris: Update included soong namespace
    We're putting everything that depends on pixelatoms behind a soong
    namespace, since hardware/google/pixel is no longer available after
    CP2A. Using a combination of prebuilts and source built things that
    depend on pixelatoms makes the build fail catastrophically, so we
    will just make everything prebuilt for supported Pixel devices.
    
    Change-Id: I3d9e1d9436f1edfbb45bca0aa709334287bb12ce
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- device.mk
+++ device.mk
@@ -312,8 +312,10 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/pixel \
     hardware/google/interfaces \
+    hardware/google/pixel/pixelstats \
+    hardware/google/pixel/power-libperfmgr \
+    hardware/google/pixel/thermal \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client
 
