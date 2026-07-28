0b257bdfd437a1449f7cadbb9d1e723202b96e0f
sreelekshman <sreelekshmanchunakara@gmail.com>
2026-07-28 10:16:32 +0530

    Tetris: Initial vendor blobs import
    Change-Id: Iac3c51f5774cbe920a02864e37eafc9213a0ad73
    Co-authored-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -28,9 +28,14 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
+    'vendor.mediatek.hardware.videotelephony@1.0': lib_fixup_vendor_suffix,
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libimsma.so': blob_fixup()
+        .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
+        .replace_needed('libalsautils.so', 'libalsautils-v33.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
@@ -39,6 +44,7 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
