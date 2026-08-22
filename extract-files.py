335a4c013b6e7452a9026a3669b45141570d2a01
Mashopy <eliasgheeraert@gmail.com>
2026-08-23 00:42:06 +0200

    Tetris: Sync vendor WPA supplicant configs with MediaTek upstream ALPS
    Both p2p_go_vht and rsn_overriding have been added into the
    vendor WPA supplicant configs since ALPS 16.

--- extract-files.py
+++ extract-files.py
@@ -52,6 +52,10 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libcodec2_hidl@1.1.so', 'libcodec2_hidl@1.1-v33.so')
         .replace_needed('libcodec2_hidl@1.2.so', 'libcodec2_hidl@1.2-v33.so')
         .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v33.so'),
+    'vendor/etc/wifi/p2p_supplicant_overlay.conf': blob_fixup()
+        .add_line_if_missing('p2p_go_vht=1'),
+    'vendor/etc/wifi/wpa_supplicant.conf': blob_fixup()
+        .add_line_if_missing('rsn_overriding=1'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libalsautils.so', 'libalsautils-v33.so')
