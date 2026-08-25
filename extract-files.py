b81405558799f590396755b0d768211e94891b63
sreelekshman <sreelekshmanchunakara@gmail.com>
2026-08-25 20:48:38 +0530

    Tetris: Add missing liblog dependency to some blobs

--- extract-files.py
+++ extract-files.py
@@ -168,6 +168,11 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/librt_extamp_intf.so',
     ): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+
+    'vendor/lib64/mt6878/lib3a.ae.stat.so': blob_fixup()
+        .add_needed('liblog.so'),
+    'vendor/lib64/libarmnn_ndk.mtk.vndk.so': blob_fixup()
+        .add_needed('liblog.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
